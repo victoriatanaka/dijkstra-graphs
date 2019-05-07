c5913395ee722863fd345574a57c3705507025ab
victoriatanaka <victoriatanaka@hotmail.com>
2019-05-07 14:24:38 -0300

    Add files via upload

---
+++ utils.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Cores *-*
+COLOR_RED='\033[0;31m'
+COLOR_GREEN='\033[0;32m'
+COLOR_YELLOW='\033[0;33m'
+COLOR_BLUE='\033[0;34m'
+COLOR_END='\033[0m'
+
+p_info()
+{
+  echo -e "${COLOR_BLUE}$*${COLOR_END}"
+}
+
+p_nome()
+{
+  echo -e "${COLOR_YELLOW}$*${COLOR_END}"
+}
+
+p_erro()
+{
+  echo -e "${COLOR_RED}$*${COLOR_END}"
+}
+
+p_ok()
+{
+  echo -e "${COLOR_GREEN}$*${COLOR_END}"
+}
