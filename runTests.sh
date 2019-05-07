ecc67a3d7c03d890bda0c3491257e59b60c2e826
victoriatanaka <victoriatanaka@hotmail.com>
2019-05-07 14:22:31 -0300

    Update runTests.sh

--- runTests.sh
+++ runTests.sh
@@ -11,14 +11,14 @@ p_info '-------------------------------------------------------------------'
 p_info 'Rodando testes'
 p_info '-------------------------------------------------------------------'
 cd src
-python main2.py dados.txt A Y
-python main2.py dados.txt G Y
-python main2.py dados.txt D U
-python main2.py dados.txt Q J
-python main2.py dados.txt V K
-python main2.py dados.txt L H
-python main2.py dados.txt T W
-python main2.py dados.txt X K
+python main2.py ../graphs/dados.txt A Y
+python main2.py ../graphs/dados.txt G Y
+python main2.py ../graphs/dados.txt D U
+python main2.py ../graphs/dados.txt Q J
+python main2.py ../graphs/dados.txt V K
+python main2.py ../graphs/dados.txt L H
+python main2.py ../graphs/dados.txt T W
+python main2.py ../graphs/dados.txt X K
 
 p_ok "\nOK. Output gerado em /Desktop/caminhos.txt"
 
